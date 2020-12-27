9779e5ed151196dad9757215f61aed0bbadaad38
lfenandomr18 <lfernandomr18@gmail.com>
2020-12-27 13:28:38 -0500

    cambios

---
+++ codigos durante el curso/9 while y eliminacaracterespecifico.py
@@ -0,0 +1,17 @@
+numero=int(input('Ingresa un número: '))
+textosalida=''
+
+def kill_char(string, n): # n = position of which character you want to remove
+    begin = string[:n]    # from beginning to n (n not included)
+    end = string[n+1:]    # n+1 through end of string
+    return begin + end
+# print kill_char("EXAMPLE", 3)  # "M" removed
+
+if numero >20:
+    print('No seas pendejo numero es muy alto...')
+else:
+    while numero>0:
+        textosalida=textosalida+str(numero)+','
+        numero=numero-1
+numero=len(textosalida)-1
+print (kill_char(textosalida,numero))
