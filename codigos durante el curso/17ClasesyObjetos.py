d2bd0b4610e653ad935f436fcd4d288662e558a0
lfenandomr18 <lfernandomr18@gmail.com>
2021-01-10 23:36:53 -0500

    añadido objetos con innit y array de players evitando nones

--- codigos durante el curso/17ClasesyObjetos.py
+++ codigos durante el curso/17ClasesyObjetos.py
@@ -4,30 +4,37 @@ from Funciones import PartidaxId
 import requests
 
 
+
 api=configuraciones.apikeyopendota
 
 salida=PartidaxId("5769042950",api)  
 
 json_dataDePartida = json.loads(salida)
 
-
+## esto es una clase
 class Partida:
     match_id=None
     barracks_status_dire=None
     barracks_status_radiant=None
     replay_url=None
+    players=[]
+    #esto permite inicializar mi objeto 
+    def __init__(self,match_id,barracks_status_dire,barracks_status_radiant,replay_url,players):
+        self.match_id=match_id
+        self.barracks_status_dire=barracks_status_dire
+        self.barracks_status_radiant=barracks_status_radiant
+        self.replay_url=replay_url
+        cont=len(players)
+        for i in players:
+            if players[cont-1]['account_id'] != None:
+                self.players.append(players[cont-1]['account_id'])
+            cont=cont-1
+            
 
-nuevapartida= Partida()
-nuevapartida.match_id=json_dataDePartida['match_id']
-nuevapartida.barracks_status_dire=json_dataDePartida['barracks_status_dire']
-nuevapartida.barracks_status_radiant=json_dataDePartida['barracks_status_radiant']
-nuevapartida.replay_url=json_dataDePartida['replay_url']
+        salida=' '.join(map(str, self.players))
+        print('se ha creado el objeto de la partida: %d, las barracas:%d,%d y los jugadores:'%(match_id,barracks_status_dire,barracks_status_radiant)+salida)
 
-print(nuevapartida.match_id)
-print(nuevapartida.barracks_status_dire)
-print(nuevapartida.barracks_status_radiant)
-print(nuevapartida.replay_url)
-
+nuevapartida= Partida(json_dataDePartida['match_id'],json_dataDePartida['barracks_status_dire'],json_dataDePartida['barracks_status_radiant'],json_dataDePartida['replay_url'],json_dataDePartida['players'])
 
 
 
